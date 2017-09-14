0a319b19c60c0104c4839d756b950ea7534822c9
Rafael Carneiro de Castro <rafaelcarneiroget@hotmail.com>
2017-09-14 14:25:12 -0300

    Operating files

--- TicTacToe/teste.py
+++ TicTacToe/teste.py
@@ -1,7 +1,9 @@
 def celsius_to_fahrenheit(temperature):
     if temperature < -273.15:
-        return "Incorrect value!"
+        return "That temperature doesn't make sense!"
     return temperature * 9.0/5.0 + 32.0
 
 
-print(celsius_to_fahrenheit(-274))
+temperatures = [10,-20,-289,100]
+for temperature in temperatures:
+    print(celsius_to_fahrenheit(temperature))
